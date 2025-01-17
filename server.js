f23dbb7483fee5f4ebcc5a16b8e3dcdd0031da3b
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-01-16 19:57:56 -0600

    console.log the req.url and the method

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 // 'http' is a module is already included in the Node JS
 import http from 'http'
-const portNumber = 3000;
+const portNumber = process.env.PORT;
 
 
 // creating a new http server
 const server = http.createServer((req, res) => {
 
     // To set a header to the contents 
-    res.setHeader('Content-Type', 'text/html');
-
+    // res.setHeader('Content-Type', 'text/html');
+    console.log(req.url) // the .url here returns the url (path)
+    console.log(req.method)
     // res.write('Hello World!') 
     //res.end('<h1> Hello World! </h1>'); // We can git rid of the res.write() by typing Hello World in res.end()
 
     // to set a status of the response to different value (ex; 404)
-    res.statusCode = 404;
+    // res.statusCode = 404;
 
     // another methode is writeHead ('error code' , {"content-type" : "applicaiton/json"})
 
-    res.writeHead (500, {"content-type" : 'application/json'})
-    res.end (JSON.stringify({message : ' ServerError'}));
+
+
+    res.writeHead (200, {"content-type" : 'text/html'})
+    res.end ("<h1>Hello World</h1>");
 })
 
 

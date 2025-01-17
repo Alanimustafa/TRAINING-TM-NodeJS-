dde8fd163deab067dd80be41bf47a953697c42b3
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-01-16 18:44:55 -0600

    Export and import through different modules

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import {generateRandomNumber, celciusToFahrenheit} from './utils.js'
-console.log("Hello World");
 
-// generateRandomNumber = require('./utils.js');
+// This import syntax when I have a const assigned to the exported function.
+// import {getPosts} from './postControler.js'
 
-console.log(`Random Numner :  ${generateRandomNumber()}`);
-console.log(`celciuse to Fahrenheit :  ${celciusToFahrenheit(0)}`);
+// This syntax of import when the funciton has a default export.
+import getPosts, {getPostLength} from './postControler.js'
+
+console.log(getPosts());
+console.log(getPostLength());
+// console.log("Hello World");
+
+// // generateRandomNumber = require('./utils.js');
+
+// console.log(`Random Numner :  ${generateRandomNumber()}`);
+// console.log(`celciuse to Fahrenheit :  ${celciusToFahrenheit(0)}`);
--- postControler.js
+++ postControler.js
@@ -2,3 +2,11 @@ const posts = [
     {id: 1, title: 'Post One' },
     {id: 2, title: 'Post Two' }
 ]
+
+// exportred fucntion return posts above.
+const getPosts = () => posts;
+
+export default getPosts;
+
+
+export const getPostLength = () => posts.length;

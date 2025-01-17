d3b561c006965e9a1bb3054e520cdcf232c2d428
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-01-16 19:13:25 -0600

    using res.writeHead instead of statusCode and setHeader

--- server.js
+++ server.js
@@ -5,8 +5,20 @@ const portNumber = 3000;
 
 // creating a new http server
 const server = http.createServer((req, res) => {
-    res.write('Hello World!')
-    res.end();
+
+    // To set a header to the contents 
+    res.setHeader('Content-Type', 'text/html');
+
+    // res.write('Hello World!') 
+    //res.end('<h1> Hello World! </h1>'); // We can git rid of the res.write() by typing Hello World in res.end()
+
+    // to set a status of the response to different value (ex; 404)
+    res.statusCode = 404;
+
+    // another methode is writeHead ('error code' , {"content-type" : "applicaiton/json"})
+
+    res.writeHead (500, {"content-type" : 'application/json'})
+    res.end (JSON.stringify({message : ' ServerError'}));
 })
 
 
